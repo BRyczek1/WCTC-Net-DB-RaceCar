92aa6e1e382b12de9b77aeb000efa0d18833f406
bryczek23 <Bryczek1@my.wctc.edu>
2023-03-13 18:46:25 -0500

    Part 1 complete

---
+++ RaceTrack/Cars/Truck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Truck : RaceCar
+    {
+        public Truck()
+        {
+            Name = "Truck";
+            TopSpeed = 85;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} is heating up!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} Screaches to a halt!");
+            base.Brake();
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Bill : Driver
+    {
+        public Bill(RaceCar car) : base(car)
+        {
+            Name = "Bill";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
